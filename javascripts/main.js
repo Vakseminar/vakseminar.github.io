4395fb8274136669ba679b0b59f727c3222416f7
Gustavo Krause <emaildokrause@gmail.com>
2014-08-05 21:35:07 -0300

    Create FE the all screen
    First implementation

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,21 @@
-console.log('This would be the main JS file.');
+
+$(document).ready(function(){
+
+  function scrollTela(id) {
+    var posicao = $(id).offset().top;
+    $('html, body').animate({ scrollTop: posicao }, 'slow');
+  }
+
+
+  // scroll
+  $('a[href^=\'#\']').click(function () {
+      var id = $(this).attr('href');
+      scrollTela(id);
+      return false;
+  });
+
+
+  // $(this).scroll(function() {
+  // });
+
+});
